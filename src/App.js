6398c047474730e708278ad5afd6d3d014ab90c1
joelvaz <jrv1271663@gmail.com>
2021-03-12 21:12:35 -0800

    CHANGE 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ThumbnailCard from './components/ThumbnailCard/ThumbnailCard.js'
 import NavBar from './components/NavBar/NavBar.js';
+import VideoShelf from './components/VideoShelf/VideoShelf.js';
 
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <VideoShelf></VideoShelf>
       <ThumbnailCard></ThumbnailCard>
     </div>
   );
---
+++ src/components/VideoShelf/VideoShelf.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class VideoShelf extends Component {
+    render() {
+        return (
+            <div className="VideoShelf">
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
