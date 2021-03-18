3a7a888f7b2401f80bae9e15f824c237bc97ccc3
joelvaz <jrv1271663@gmail.com>
2021-03-17 20:20:54 -0700

    change 5

--- src/components/ThumbnailCard/ThumbnailCard.js
+++ src/components/ThumbnailCard/ThumbnailCard.js
@@ -1,18 +1,28 @@
 import './ThumbnailCard.css';
+
 import ThumbnailPicture from '../../assets/yt.png';
 import React, { Component } from 'react';
 
 class ThumbnailCard extends Component {
-    componentDidMount(){
-        var container_width = document.getElementById("ThumbnailCard").offsetWidth;
-        var pic_height = container_width * 0.618033989;
-        document.getElementById("thumbnailPicture").style.height = pic_height + "px";
-    }
+
+    
+    constructor() {
+        super();
+        this.state = { height: 0};
+      }
+
+      componentDidMount() {
+        var height = parseInt(document.getElementsByClassName('ThumbnailCard')[0].offsetWidth) * 0.618033989;
+        this.setState({ height: height });
+      }
+
+      
     render() {
+
         return (
-            <div id="ThumbnailCard">
+            <div className="ThumbnailCard">
                 <a href="#" className="video-top-section" data-duration="12:24">
-                    <img id="thumbnailPicture" src={ThumbnailPicture}></img>
+                    <img id="thumbnailPicture" style={{height: this.state.height}} src={ThumbnailPicture}></img>
                 </a>
                 <div className="video-bottom-section">
                     <a href="#">
--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,11 +1,20 @@
+import './VideoSection.css';
 import React, { Component } from 'react';
 import ThumbnailCard from '../ThumbnailCard/ThumbnailCard.js';
 
 export default class VideoSection extends Component {
     render() {
         return (
-            <div>
-                <ThumbnailCard></ThumbnailCard>
+            <div className="VideoSection">
+                <div id="container">
+                    <div><ThumbnailCard className="ThumbnailCard"></ThumbnailCard></div>
+                    <div><ThumbnailCard className="ThumbnailCard"></ThumbnailCard></div>
+                    <div><ThumbnailCard className="ThumbnailCard"></ThumbnailCard></div>
+                    <div><ThumbnailCard className="ThumbnailCard"></ThumbnailCard></div>
+                    <div><ThumbnailCard className="ThumbnailCard"></ThumbnailCard></div>
+                    <div><ThumbnailCard className="ThumbnailCard"></ThumbnailCard></div>
+
+                </div>
             </div>
         )
     }
